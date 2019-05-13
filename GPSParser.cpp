36dfeea8862a8e41721ab21536e9543a8bcce063
benjam60 <erichseb@umich.edu>
2019-05-12 21:58:34 -0400

    refactoring and made more memory safe

--- GPSParser.cpp
+++ GPSParser.cpp
@@ -11,55 +11,59 @@ struct GPSCoordinates {
 
 //TODO BE: cleanup memory
 //TODO BE: assert only one message per read
+//TODO BE: Add asserts nth != 0
 class GPSParser {
-	SystemGPS * gps;
-	char NOENTRY[5] = "-1.0";
-	const int NoEntrySize = 1;
 public:
 	GPSParser(SystemGPS * gpsIn) : gps(gpsIn) { }
 
 	struct GPSCoordinates waitAndGetNextPosition() {
 	   struct GPSCoordinates gpsCoordinates;
 	   while (gpsCoordinates.latitude == -1.0f) {
-		   char * gpsInput = gps->waitAndReadData();
-		   if ((strstr(gpsInput, "$GPRMC") != NULL) && gprmcNmeaMessageIsValid(gpsInput)) {
-			   float latitude = atof(getCSVEntry(gpsInput, 3));
-			   gpsCoordinates.latitude = latitude;
+		   char * nmeaSentence = gps->waitAndReadData();
+		   if ((strstr(nmeaSentence, "$GPRMC") != NULL) && gprmcNmeaMessageIsValid(nmeaSentence)) {
+			   char * latitude = getCSVEntry(nmeaSentence, IndexOfCommaForLatitude);
+			   gpsCoordinates.latitude = atof(latitude);
+			   delete latitude;
 		   }
 	   }
 	   return gpsCoordinates;
 	}
 
-	bool gprmcNmeaMessageIsValid(const char * nmeaMessage) {
+private:
+	bool gprmcNmeaMessageIsValid(const char * nmeaMessage) const {
 		int numCommas = 0;
 		for (int i = 0; i < strlen(nmeaMessage); ++i) { //TODO: do not go on forever
 			if (nmeaMessage[i] == ',') { ++numCommas; }
 		}
 		const int ExpectedCommas = 12;
-		return (numCommas == ExpectedCommas);
+		return numCommas == ExpectedCommas;
 	}
 
 	char * getCSVEntry(char * csvRow, int nth) {
-		//assert(nth != 0);
-		const char * startingIndexOfEntry = getIndexOfComma(csvRow, nth);
-		const char * endingIndexOfEntry = getIndexOfComma(csvRow, nth+1); //account for /n/r at end
-		int entrySize = endingIndexOfEntry - startingIndexOfEntry;
-		if (entrySize == NoEntrySize) { return NOENTRY; }
-		else {
+		const char * startingIndexOfEntry = findComma(csvRow, nth) + 1;
+		const char * exclusiveEndingIndexOfEntry = findComma(csvRow, nth+1); //account for /n/r at end
+		const int entrySize = exclusiveEndingIndexOfEntry - startingIndexOfEntry;
+		if (entrySize != 0)  {
 			char * outputBuffer = new char[entrySize];
-			strncpy(outputBuffer, startingIndexOfEntry + 1, entrySize - 1); //handle minus and place one in case empty or 1 char
-			return outputBuffer;
+			return strncpy(outputBuffer, startingIndexOfEntry, entrySize);
+		}
+		else {
+			char * outputBuffer = new char[5];
+			return strcpy(outputBuffer, "-1.0");
 		}
 	}
 
-    const char * getIndexOfComma(char * csvRow, int nth) {
+    const char * findComma(char * csvRow, int nth) {
 		int numOfCommasSeen = 0;
-		for(char * index = csvRow; (char)index[0] != '\n'; ++index) { //handle wrong input
+		for(char * index = csvRow; (char)index[0] != '\0'; ++index) { //handle wrong input
 			if ((char)index[0] == ',') ++numOfCommasSeen;
 			if (numOfCommasSeen == nth) return index;
 		}
-		return nullptr; //handle
+		return NULL; //handle
 	}
+
+	SystemGPS * gps;
+	const int IndexOfCommaForLatitude = 3;
 };
 
 #endif
