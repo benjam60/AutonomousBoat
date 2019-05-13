c64585c69ab3bdc0160f0c32437bc8fc7d3eefaf
benjam60 <erichseb@umich.edu>
2019-05-12 22:19:02 -0400

    refactoring

--- GPSParser.cpp
+++ GPSParser.cpp
@@ -19,7 +19,7 @@ public:
 	struct GPSCoordinates waitAndGetNextPosition() {
 	   struct GPSCoordinates gpsCoordinates;
 	   while (gpsCoordinates.latitude == -1.0f) {
-		   char * nmeaSentence = gps->waitAndReadData();
+		   const char * nmeaSentence = gps->waitAndReadData();
 		   if ((strstr(nmeaSentence, "$GPRMC") != NULL) && gprmcNmeaMessageIsValid(nmeaSentence)) {
 			   char * latitude = getCSVEntry(nmeaSentence, IndexOfCommaForLatitude);
 			   gpsCoordinates.latitude = atof(latitude);
@@ -30,16 +30,17 @@ public:
 	}
 
 private:
-	bool gprmcNmeaMessageIsValid(const char * nmeaMessage) const {
+	const bool gprmcNmeaMessageIsValid(const char * const nmeaMessage) const {
 		int numCommas = 0;
-		for (int i = 0; i < strlen(nmeaMessage); ++i) { //TODO: do not go on forever
+		const int NmeaMessageLength = strlen(nmeaMessage);
+		for (int i = 0; i < NmeaMessageLength; ++i) { //TODO: do not go on forever
 			if (nmeaMessage[i] == ',') { ++numCommas; }
 		}
 		const int ExpectedCommas = 12;
-		return numCommas == ExpectedCommas;
+		return ExpectedCommas == numCommas;
 	}
 
-	char * getCSVEntry(char * csvRow, int nth) {
+	char * getCSVEntry(const char * csvRow, const int nth) const {
 		const char * startingIndexOfEntry = findComma(csvRow, nth) + 1;
 		const char * exclusiveEndingIndexOfEntry = findComma(csvRow, nth+1); //account for /n/r at end
 		const int entrySize = exclusiveEndingIndexOfEntry - startingIndexOfEntry;
@@ -53,10 +54,10 @@ private:
 		}
 	}
 
-    const char * findComma(char * csvRow, int nth) {
+    const char * findComma(const char * csvRow, int nth) const { //TODO BE: find out if should be passing in const pointer
 		int numOfCommasSeen = 0;
-		for(char * index = csvRow; (char)index[0] != '\0'; ++index) { //handle wrong input
-			if ((char)index[0] == ',') ++numOfCommasSeen;
+		for(const char * index = csvRow; (char)index[0] != '\0'; ++index) { //handle wrong input
+			if (',' == index[0]) ++numOfCommasSeen;
 			if (numOfCommasSeen == nth) return index;
 		}
 		return NULL; //handle
